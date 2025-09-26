a9f90f2398ea47f2181153e7ff7ea5f3f376f838
Mithunraj Kurunthil <103574185+MithunrajKurunthil@users.noreply.github.com>
2025-09-27 00:40:02 +0530

    Add files via upload

---
+++ equipo/pythonapi/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import mysql.connector
+import settings
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return {"message": "Hello from Python API!"}
+
+@app.route("/db-check")
+def db_check():
+    try:
+        conn = mysql.connector.connect(
+            host=settings.DB_HOST,
+            user=settings.DB_USER,
+            password=settings.DB_PASSWORD,
+            database=settings.DB_NAME
+        )
+        conn.close()
+        return jsonify({"db_status": "Connected"})
+    except Exception as e:
+        return jsonify({"db_status": "Error", "details": str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
---
+++ equipo/reactapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello Equipo</h1>
+      <p>This React app is served by Nginx inside a Docker container.</p>
+    </div>
+  );
+}
+
+export default App;
+
